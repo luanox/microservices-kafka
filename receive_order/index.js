61407fb7d72d6038e84d9b5514fb99f9663b8818
Luan Neves <luanuemg@hotmail.com>
2021-07-28 18:36:11 -0300

    feat(receive_order): created topic and send message in kafka

--- receive_order/index.js
+++ receive_order/index.js
@@ -7,6 +7,11 @@ const app = express();
 const {Kafka} = require('kafkajs');
 app.use(express.json());
 
+const kafka = new Kafka({
+  clientId: 'teste_rede',
+  brokers: ['localhost:9092']
+})
+
 // Middleware
 
 
@@ -15,6 +20,18 @@ app.post("/receive_order",async (req, res) => {
   const order = req.body;
   order.id = uuidv4();
   order.status = "PENDENTE_PAGAMENTO";
+
+  const producer = kafka.producer()
+
+  await producer.connect()
+  await producer.send({
+    topic: 'RECEIVE_ORDER',
+    messages: [
+      {value: JSON.stringify(order)}
+    ]
+  })
+
+  await producer.disconnect()
 
   return res.status(201).json(order);
 })
